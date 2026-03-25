4f983e60deaa6e8d8ef1a72ee0da71cb31871245
Karime Natalia Torres Bautista <karime.torres.23e@utzmg.edu.mx>
2026-03-24 22:25:17 -0600

    Configure dynamic database path for Railway

--- src/db/database.js
+++ src/db/database.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const dbPath = path.join(__dirname, '..', '..', 'pokedex.db');
+const dbPath = process.env.DB_PATH || path.join(__dirname, '..', '..', 'pokedex.db');
 const db = new Database(dbPath);
 
 // Enable WAL mode for better concurrency
